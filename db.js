32423d259b04c563b472307e5328a404ed35d680
Chris Huber <c.c.a.huber@gmail.com>
2019-10-27 21:51:58 -0700

    Finished reg validation, starting Database connection setup

---
+++ db.js
@@ -0,0 +1,5 @@
+const mongodb = require('mongodb');
+
+mongodb.connect(connectionString, {useNewUrlParser: true, useUnifiedTopology: true}, (err, client) => {
+
+});
--- models/User.js
+++ models/User.js
@@ -18,6 +18,8 @@ User.prototype.register = function() {
     // 2. Only if no validation errors - Save user data into database
  
 }; */
+console.log();
+const validator = require('validator');
 
 class User {
     
@@ -28,26 +30,67 @@ class User {
 
     register() {
         console.log('register working');
-        // 1. Validate user data
+        // 1. Clean registration data as only text string. Then validate reg meets requirements.
+        this.valCleanUp();
         this.validate();
         // 2. Only IF there are NO validation err - Save user data to DB
     };
 
+    valCleanUp() {
+    // 
+        if (typeof(this.data.username != 'string')) {
+            this.data.username = '';
+        };
+        if (typeof (this.data.email != 'string')) {
+            this.data.email = '';
+        };
+        if (typeof (this.data.password != 'string')) {
+            this.data.password = '';
+        };
+
+        // Get rid of unwanted properties in our object. Reset object to properties we want.
+        // This prevents users from submitting their own data to the object.
+        // .trim removes blank spaces.
+        // Passwords should include upper or lower case and spaces if desired.
+        this.data = {
+            username: this.data.username.trim().toLowerCase(),
+            email: this.data.email.trim().toLowerCase(),
+            password: this.data.password
+        };
+    };
+
     validate() {
-        if (this.data.username == '') {
-            this.errors.push('You must provide a username.');
-            console.log('validation 1');
-        }
+    // Validation can be done by useful packages or using regular expressions.
+ 
+        // Validate username: No blank field | Must be only alphanumeric 
+        if (this.data.username != '' && !validator.isAlphanumeric(this.data.username)) {
+            this.errors.push('Username must contain only letters or numbers.');
+        };
+        if (this.data.username.length > 0 && this.data.username.length < 3) {
+            this.errors.push('username must be greater than 3 characters');
+        };
 
-        if (this.data.email == '') {
-            this.errors.push('You must provide an email address.');
-            console.log('validation 2');
-        }
+        if (this.data.username.length > 100) {
+            this.errors.push('Password cannot be longer than 100 characters.');
+        };
 
+        // No blank field | Must be a valid email address using @ symbol
+        if (!validator.isEmail(this.data.email)) {
+            this.errors.push('You must provide a valid email address.');
+        };
+        // No blank field | character over and under limit.
         if (this.data.password == '') {
             this.errors.push('You must provide a password');
             console.log('validation 3');
-        }
+        };
+
+        if (this.data.password.length > 0 && this.data.password.length < 12) {
+            this.errors.push('Password must be greater than 12 characters.');
+        };
+
+        if (this.data.password.length > 100) {
+            this.errors.push('Password cannot be longer than 100 characters.');
+        };
     };
 }
 
